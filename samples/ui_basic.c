896875ecf08a1f26c63dc4b4840d39cc84624d81
Michael Dodis <michaeldodisgr@gmail.com>
2025-08-31 10:02:32 +0200

    add some opengl 2 fucntions

--- samples/ui_basic.c
+++ samples/ui_basic.c
@@ -11,12 +11,23 @@ int main(int argc, char const *argv[])
 
     while (vd_fw_running()) {
 
-        vd_fw_begin_render();
+        // vd_fw_begin_render();
 
+        int w, h;
+        vd_fw_get_size(&w, &h);
+
+        glViewport(0, 0, w, h);
         glClearColor(0.5f * (sinf(d) + 1.f) , 0.2f, 0.9f, 0.f);
-        glClear(0x00004000);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        vd_fw_end_render();
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.3f, 0.4f);
+        glVertex2f(0.f, 0.f);
+        glVertex2f(0.5f, 1.f);
+        glVertex2f(1.f, 1.f);
+        glEnd();
+
+        // vd_fw_end_render();
         vd_fw_swap_buffers();
 
         d += 0.001f;
