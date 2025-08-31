69d8164c830e6b7af20efe2432e334992a24c710
Michael Dodis <michaeldodisgr@gmail.com>
2025-08-31 08:29:57 +0200

    fix build.bat directories

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -3,13 +3,15 @@ setlocal EnableDelayedExpansion
 set SCRIPT_DIR=%~dp0
 
 pushd %SCRIPT_DIR%\..\..\
+set PRJ_DIR=%cd%
 if not exist build mkdir build
 pushd build
 
 REM samples/ui_basic.c------------------
-set SRC_DIR=..\samples
+set INC_DIR=%PRJ_DIR%
+set SRC_DIR=%PRJ_DIR%\samples
 
-cl /Zi /Od /nologo %SRC_DIR%\ui_basic.c /Fe:ui_basic.exe /link OpenGL32.lib
+cl /Zi /Od /I %INC_DIR% /W4  /nologo %SRC_DIR%\ui_basic.c /Fe:ui_basic.exe /link User32.lib OpenGL32.lib
 REM samples/ui_basic.c------------------
 
 popd
