67cf2c20efcae1282d2a91f164fdba400400b041
Michael Dodis <michaeldodisgr@gmail.com>
2026-03-09 08:10:14 +0100

    vd_fw.h: test dx11

--- scripts/windows/test.bat
+++ scripts/windows/test.bat
@@ -8,9 +8,9 @@ if not exist build-test mkdir build-test
 pushd build-test
 set INC_DIR=%PRJ_DIR%
 set SRC_DIR=%PRJ_DIR%
-set CFLAGS=/Zi /Od /W4 /GS- /nologo /I %PRJ_DIR% /I %PRJ_DIR%\ext
+set CFLAGS=/Zi /Od /W4 /std:c++20 /GS- /nologo /I %PRJ_DIR% /I %PRJ_DIR%\ext
 
-cl %CFLAGS% %SRC_DIR%\tests\vd_fw_test.c
+cl %CFLAGS% %SRC_DIR%\tests\vd_fw_test.cpp
 
 popd
 popd
